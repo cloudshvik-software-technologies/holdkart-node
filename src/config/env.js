12dab7455819490c292228b7e686f91bfc40f9d9
sindhu-cst <sindhu@cloudshvik.com>
2026-06-08 15:31:00 +0530

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
   export default {
-    port:            process.env.PORT             || 8081,
-    jwtSecret:       process.env.JWT_SECRET       || 'customer_access_secret',
-    jwtRefreshSecret:process.env.JWT_REFRESH_SECRET || 'customer_refresh_secret',
-    frontendUrl:     process.env.FRONTEND_URL     || 'http://localhost:5174/',
+    port:            process.env.PORT,
+    jwtSecret:       process.env.JWT_SECRET,
+    jwtRefreshSecret:process.env.JWT_REFRESH_SECRET,
+    frontendUrl:     process.env.FRONTEND_URL,
     razorpayKeyId:   process.env.RAZORPAY_KEY_ID,
     razorpaySecret:  process.env.RAZORPAY_KEY_SECRET,
   };
